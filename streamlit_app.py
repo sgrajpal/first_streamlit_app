90c5a4880d90c69111d8fe005b296c1c6b4ee417
sgrajpal <sgrajpal@gmail.com>
2022-10-26 04:12:58 -0400

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,4 @@
 import streamlit
-streamlit.title ('My Parents NEw Healthy diner')
+streamlit.title ('My Parents NEW Healthy diner')
+streamlit.header ('Breakfast manu')
+streamlit.text ('Egg and cheez')
